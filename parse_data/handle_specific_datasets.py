46f2433bb6520c9c779facc9e5815139b66a3a4c
= <tomhagley@btinternet.com>
2024-10-22 17:11:34 +0100

    cleaned up parse_data/handle_specific_datasets

--- parse_data/handle_specific_datasets.py
+++ parse_data/handle_specific_datasets.py
@@ -27,39 +27,13 @@ json_normalise = True
 # In[3]:
 
 
-## paths
-# data_folder = '/home/tom/Documents/SWC/data' # desktop Duan Lab
-# json_filename = '240913_Yansu_Jerry/2024-09-13_11-23-37_YansuFirstSolo.json' 
-data_folder = r'D:\Users\Tom\OneDrive\PhD\SWC\data' # desktop home
-json_filename = r'first_experiments_2409\240913\2024-09-13_11-23-37_YansuFirstSolo.json'
+# ## paths
+# # data_folder = '/home/tom/Documents/SWC/data' # desktop Duan Lab
+# # json_filename = '240913_Yansu_Jerry/2024-09-13_11-23-37_YansuFirstSolo.json' 
+# data_folder = r'D:\Users\Tom\OneDrive\PhD\SWC\data' # desktop home
+# json_filename = r'first_experiments_2409\240913\2024-09-13_11-23-37_YansuFirstSolo.json'
 
-filepath = data_folder + os.sep + json_filename
-
-
-# In[4]:
-
-
-# Note json_normalize requires the json file, whereas read_json requires the filepath
-if json_normalise == True:
-    with open(filepath) as f:
-        file = json.load(f)
-        df = pd.json_normalize(file)
-else:
-    with open(filepath) as f:
-        df = pd.read_json(f)
-        print(type(df))
-
-
-# In[5]:
-
-
-##  Convert time columns into datetime format
-df['timeLocal'] = pd.to_datetime(df['timeLocal'], format='%H:%M:%S:%f')
-
-# Use to_timedelta instead as a vectorised function (lambdas are python loops)
-# df['timeApplication'] = df['timeApplication'].apply(lambda x: timedelta(seconds=int(x) + (x - int(x))))
-df['timeApplication'] = pd.to_numeric(df['timeApplication']) 
-df['timeApplication'] = pd.to_timedelta(df['timeApplication'], unit='s')
+# filepath = data_folder + os.sep + json_filename
 
 
 # In[52]:
@@ -97,31 +71,7 @@ def playerinfo_playerposition_conversion(df, solo=True):
     return df2
 
 
-# In[10]:
-
-
-df[df['data.wall1'] == 0]['data.wall1']
-
-
-# In[12]:
-
-
-df2 = df.copy()
-
-
-# In[14]:
-
-
-df2[df2['data.wall1'] == 0] = np.nan
-
-
-# In[16]:
-
-
-df2['data.wall1'].iloc[4097]
-
-
-# In[17]:
+# In[18]:
 
 
 def remove_zero_wall_numbers(df):
@@ -133,16 +83,4 @@ def remove_zero_wall_numbers(df):
     df2[df2['data.wall2'] == 0] = np.nan
 
     return df2
-
-
-# In[58]:
-
-
-df2 = playerinfo_playerposition_conversion(df, solo=True)
-
-
-# In[59]:
-
-
-df2.tail()
 
