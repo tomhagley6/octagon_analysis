eda0ffd7c17a8693f736fd4aa3fc9a308e661166
bennylupattelli <benedetta.gencarelli.24@ucl.ac.uk>
2024-10-25 12:46:35 +0100

    Updated plot_cones to mark active walls in each trial

--- plot_cones.py
+++ plot_cones.py
@@ -51,8 +51,8 @@ def plot_cones(ax, trial_list=None, trial_index=0, trial=None):
         cone_angle_right = rot_rad + np.pi / 4  # 45 degrees to the right
 
         # Endpoints of the cone
-        cone_base_left = (x + 2*diameter * np.cos(cone_angle_left), y + 2*diameter * np.sin(cone_angle_left))
-        cone_base_right = (x + 2*diameter * np.cos(cone_angle_right), y + 2*diameter * np.sin(cone_angle_right))
+        cone_base_left = (x + diameter * np.cos(cone_angle_left), y + diameter * np.sin(cone_angle_left))
+        cone_base_right = (x + diameter * np.cos(cone_angle_right), y + diameter * np.sin(cone_angle_right))
 
         # Plot the cone as a triangle
         ax.fill([x, cone_base_left[0], cone_base_right[0]], 
@@ -65,7 +65,8 @@ def plot_cones(ax, trial_list=None, trial_index=0, trial=None):
 
 import parse_data.prepare_data as prepare_data
 import globals
-from plotting import plot_octagon
+from plotting import plot_octagon_updated
+from assign_coordinates_to_walls import alcove_coordinates
 import numpy as np
 import pandas as pd
 import scipy
@@ -82,7 +83,24 @@ df, trial_list = prepare_data.prepare_data(data_folder, json_filenames)
 
 for i in range(len(trial_list)):
   fig, ax = plt.subplots()
-  ax = plot_octagon.plot_octagon(ax)
+  ax, alcove_x, alcove_y = plot_octagon_updated.plot_octagon(ax)
+  this_trial = extract_trial(trial=None, trial_list=trial_list, trial_index=i)
+  slice_onset_event = this_trial[this_trial['eventDescription'] == globals.SLICE_ONSET]
+  if not slice_onset_event.empty:
+     slice_onset_idx = slice_onset_event.index[0]
+
+  active_walls = df.loc[slice_onset_idx, ['data.wall1', 'data.wall2']].values
+  print("Active Walls (as integers):", active_walls)
+
+  for wall in active_walls:
+        wall = int(wall)
+        if wall in alcove_coordinates:
+            x_coords, y_coords = alcove_coordinates[wall]
+            print(f"Filling wall {wall} with coordinates: {x_coords}, {y_coords}")
+            ax.fill(x_coords, y_coords, alpha=0.5, color='red', linewidth=5)
+        else:
+            print(f"Wall {wall} not found in alcove_coordinates.")
+    
   ax = plot_cones(ax, trial_list=trial_list, trial_index = i)
   ax.set_title("xdfghjk")
 
