5cb7db4efeb1f14a498cb9cf37e3f2901ac4402f
tom <tomhagley@btinternet.com>
2024-10-26 18:24:23 +0100

    trajectory direction alignment to wall functions bugfixed. Started working on head angle throughout trajectory, and identifying whether walls fall within FoV

--- .ipynb_checkpoints/globals-checkpoint.py
+++ .ipynb_checkpoints/globals-checkpoint.py
@@ -19,6 +19,15 @@ PLAYER_1_XLOC = 'data.playerPosition.1.location.x'
 PLAYER_0_YLOC = 'data.playerPosition.0.location.z'
 PLAYER_1_YLOC = 'data.playerPosition.1.location.z'
 
+PLAYER_0_XROT = 'data.playerPosition.0.rotation.x'
+PLAYER_1_XROT = 'data.playerPosition.1.rotation.x'
+PLAYER_0_YROT = 'data.playerPosition.0.rotation.y'
+PLAYER_1_YROT = 'data.playerPosition.1.rotation.y'
+PLAYER_0_ZROT = 'data.playerPosition.0.rotation.z'
+PLAYER_1_ZROT = 'data.playerPosition.1.rotation.z'
+
+
+
 WALL_1 = 'data.wall1'
 WALL_2 = 'data.wall2'
 WALL_3 = 'data.wall3'
@@ -35,6 +44,11 @@ HIGH_LOW = 'HighLow'
 PLAYER_LOC_DICT = {
                      0: {'xloc': PLAYER_0_XLOC, 'yloc': PLAYER_0_YLOC},
                      1: {'xloc': PLAYER_1_XLOC, 'yloc': PLAYER_1_YLOC}
+                  } 
+
+PLAYER_ROT_DICT = {
+                     0: {'xrot': PLAYER_0_XROT, 'yrot': PLAYER_0_YROT, 'zrot': PLAYER_0_ZROT},
+                     1: {'xrot': PLAYER_1_XROT, 'yrot': PLAYER_1_YROT, 'zrot': PLAYER_1_ZROT}
                   } 
 
 XLOC = 'location.x'
