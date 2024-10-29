227dfb496652cf650156a3f06f8f666212767ef5
bennylupattelli <benedetta.gencarelli.24@ucl.ac.uk>
2024-10-29 18:14:03 +0000

    small fixes - still not functional

--- bennys_fishtank/compare_dist.py
+++ bennys_fishtank/compare_dist.py
@@ -33,32 +33,36 @@ def extract_trial(trial, trial_list, trial_index):
 # Key functions
 def direct_distance(this_trial, alcove_center, trial_index):
   
-  slice_onset_event = this_trial[this_trial['eventDescription'] == globals.SLICE_ONSET]
-  trigger_event = this_trial[this_trial['eventDescription'] == globals.SELECTED_TRIGGER_ACTIVATION]
-  wall_triggered = this_trial.loc[trigger_event.index, 'data.wallTriggered'].values[0]
- 
-  slice_onset_idx = slice_onset_event.index[0]
-  slice_onset_idx = int(slice_onset_idx - this_trial.index[0])
-  trigger_idx = trigger_event.index[0]
+    this_trial = extract_trial(trial, trial_list, trial_index)
 
-  num_players = preprocess.num_players(this_trial)
-  
-  coordinates = []
-  for i in range(num_players):
-    coordinates.extend((globals.PLAYER_LOC_DICT[i]['xloc'], globals.PLAYER_LOC_DICT[i]['yloc'])) 
-  
-  coordinates = {label: this_trial[label].values[slice_onset_idx:] for label in coordinates}
+    # isolate slice onset event, trigger event, and activating client
+    slice_onset_event = this_trial[this_trial['eventDescription'] == globals.SLICE_ONSET]
+    trigger_event = this_trial[this_trial['eventDescription'] == globals.SELECTED_TRIGGER_ACTIVATION]
+    trigger_activating_client = trigger_event[globals.TRIGGER_CLIENT].values[0]
+    
+    # find index of slice onset and trigger event normalised to this trial
+    slice_onset_idx = slice_onset_event.index[0]
+    slice_onset_idx = int(slice_onset_idx - this_trial.index[0])
+    trigger_idx = trigger_event.index[0]
+    trigger_idx = int(trigger_idx - this_trial.index[0])
 
-  starting_positions = []
-  for i in range(num_players):
+    num_players = preprocess.num_players(this_trial)
+  
+    coordinate_array_labels = []
+    for i in range(num_players):
+        coordinate_array_labels.extend((globals.PLAYER_LOC_DICT[i]['xloc'], globals.PLAYER_LOC_DICT[i]['yloc'])) 
+    coordinate_arrays = {label : this_trial[label].values[slice_onset_idx:] for label in coordinate_array_labels}
+    
+    starting_positions = []
+    for i in range(num_players):
         x = coordinates[globals.PLAYER_LOC_DICT[i]['xloc']][0]  # Player's x position
         y = coordinates[globals.PLAYER_LOC_DICT[i]['yloc']][0]  # Player's y position
         starting_position = np.array([x, y])
         starting_positions.append(starting_position)
 
-  alcove_centers = {}
-  num_walls = 8
-  for i in range(num_walls):
+    alcove_centers = {}
+    num_walls = 8
+    for i in range(num_walls):
         # Get the x and y coordinates for the current wall
         x1, x2 = alcove_x[i * 2], alcove_x[i * 2 + 1]
         y1, y2 = alcove_y[i * 2], alcove_y[i * 2 + 1]
@@ -68,11 +72,11 @@ def direct_distance(this_trial, alcove_center, trial_index):
         center_y = (y1 + y2) / 2
         
         # Store the center coordinates
-        alcove_centers[i + 1] = (center_x, center_y)
+        alcove_center[i + 1] = (center_x, center_y)
 
   
-  distances = [np.linalg.norm(np.array(alcove_center)-starting_position) for starting_position in starting_positions]
-  return distances
+    distances = [np.linalg.norm(np.array(alcove_center)-starting_position) for starting_position in starting_positions]
+    return distances
 
 def path_distance(trajectory):
   return np.sum(np.linalg.norm(np.diff(trajectory, axis=0), axis=1))
